e23c3d782d86d84b967dc70b5f683379fc9fcb93
cmstucke <cmstucke@gmail.com>
2024-01-24 21:24:06 -0500

    main page jsx pt 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
         <h1>[page subheading]</h1>
       </section>
       <section id='calls-to-action'>
-        <div className='color-splash'>
+        <div className='call-splash'>
           <h2 className='call-text'>our missision</h2>
         </div>
-        <div className='color-splash'>
+        <div className='call-splash'>
           <h2 className='call-text'>events</h2>
         </div>
-        <div className='color-splash'>
+        <div className='call-splash'>
           <h2 className='call-text'>get involved</h2>
         </div>
       </section>
@@ -30,6 +30,38 @@ export default function Home() {
       </section>
       <section id='news'>
         <h2 className='main-subheading'>[mission continued]</h2>
+      </section>
+      <section id='news'>
+        <h2 className='main-subheading'>upcoming events</h2>
+      </section>
+      <section id='join'>
+        <div id='join-splash'>
+          <h3>join us</h3>
+          <button className='main-button'>[join button]</button>
+        </div>
+        <div className='join-image'>
+          <h3>[join image]</h3>
+        </div>
+      </section>
+      <section id='main-info'>
+        <div id='main-contacts'>
+          <h4>[site contacts]</h4>
+        </div>
+        <div className='spray-notification'>
+          <h5>[spray notification signup]</h5>
+          <form>
+            <input
+              id='spray-notification-input'
+              type='email'
+            />
+            <button
+              className='main-button'
+            >[email submit]</button>
+          </form>
+        </div>
+        <div className='additional'>
+          <h5>[additional links]</h5>
+        </div>
       </section>
     </>
   );
