93eb4703f5bf2ffa10ffa163d4b741aa15256fe4
cmstucke <cmstucke@gmail.com>
2024-01-24 21:03:33 -0500

    main page jsx pt 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,32 @@ import { Button } from '@nextui-org/button';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <section id='main-image'>
+        <h1>[page subheading]</h1>
+      </section>
+      <section id='calls-to-action'>
+        <div className='color-splash'>
+          <h2 className='call-text'>our missision</h2>
+        </div>
+        <div className='color-splash'>
+          <h2 className='call-text'>events</h2>
+        </div>
+        <div className='color-splash'>
+          <h2 className='call-text'>get involved</h2>
+        </div>
+      </section>
+      <section id='news'>
+        <h2 className='main-subheading'>news</h2>
+      </section>
+      <section id='read-more'>
+        <div className='main-button'>
+          <p className='main-button-text'>read more</p>
+        </div>
+      </section>
+      <section id='news'>
+        <h2 className='main-subheading'>[mission continued]</h2>
+      </section>
+    </>
   );
 }
