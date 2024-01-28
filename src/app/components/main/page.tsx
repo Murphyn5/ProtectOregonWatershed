b9f1cdd0c1cc14de44a4af2e53a8a605a27f9b13
cmstucke <cmstucke@gmail.com>
2024-01-28 16:10:14 -0500

    main image fixed position

--- src/app/components/main/page.tsx
+++ src/app/components/main/page.tsx
@@ -1,21 +1,27 @@
 const Main = () => {
-  const imageStyle = {
-    // backgroundImage: `url(${mainBackground})`
-    height: "48rem"
-  };
+  // const imageStyle = {
+  //   // backgroundImage: `url(${mainBackground})`
+  //   height: "48rem"
+  // };
 
   const defaultSection = "flex justify-center w-screen"
   const subHeading = "text-darkText text-7xl font-semibold m-0 p-0 mb-5";
   const whiteParagraph = "text-white text-xl"
   const callText = "text-white text-5xl font-bold my-8 p-0";
-  const callSplash = "flex auto w-full justify-center"
+  const callSplash = "flex auto w-full justify-center box-border"
   const readMore = "bg-midnightGreen px-5 py-2 mb-5"
 
   return (
     <>
-      <section className="relative flex justify-center items-end w-screen bg-cover bg-[url('/main.jpg')] bg-center" style={imageStyle}>
-        <h1 className="flex justify-center text-white text-nowrap absolute text-7xl font-bold w-full m-0 mb-20"
-        >[page subheading]</h1>
+      <section className="relative w-screen h-96 m-0 p-0 flex items-center">
+        <img
+          src="/main.jpg"
+          alt="main image"
+          className="w-full h-full object-cover fixed -z-10 m-0 p-0"
+        />
+        <h1 className="w-full flex justify-center text-white text-nowrap relative text-7xl font-bold self-end bottom-12">
+          [page subheading]
+        </h1>
       </section>
       <section className="flex justify-evenly w-screen">
         <div className={`${callSplash} bg-midnightGreen`}>
@@ -28,16 +34,16 @@ const Main = () => {
           <h2 className={callText}>get involved</h2>
         </div>
       </section>
-      <section className={`${defaultSection}`}>
+      <section className={`${defaultSection} bg-white`}>
         <h2 className={`${subHeading}`}>news</h2>
       </section>
-      <section className={`${defaultSection}`}>
+      <section className={`${defaultSection} bg-white`}>
         <button className={`${readMore} ${whiteParagraph}`}>read more</button>
       </section>
-      <section className={`${defaultSection} bg-persianGreen py-16 mb-5`}>
+      <section className={`${defaultSection} bg-persianGreen py-16`}>
         <h2 className="text-white text-5xl italic">[mission continued]</h2>
       </section>
-      <section className={`${defaultSection} mb-5`}>
+      <section className={`${defaultSection} bg-white py-5`}>
         <h2 className={`${subHeading}`}>upcoming events</h2>
       </section>
       <section className="flex w-screen">
