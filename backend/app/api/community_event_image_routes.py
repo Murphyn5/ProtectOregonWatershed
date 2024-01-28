0b8051cf8bae7646a877cc8d230d84a721b5a0e7
nick <nlimurphy@gmail.com>
2024-01-27 17:37:38 -0700

    added community event image routes

---
+++ backend/app/api/community_event_image_routes.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify, request
+from app.models import db, CommunityEventImage
+from .auth_routes import validation_errors_to_error_messages
+from flask_login import current_user, login_required
+
+community_event_image_routes = Blueprint('community_event_images', __name__)
+
+# DELETE AN IMAGE
+@community_event_image_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def images_delete(id):
+    image = CommunityEventImage.query.get(id)
+    if not image:
+        return {
+            "errors": "Community Event Image couldn't be found",
+            "status_code": 404
+        }, 404
+
+    db.session.delete(image)
+    db.session.commit()
+    return {
+        "message": "Successfully deleted",
+        "status_code": 200
+    }
+
+# GET IMAGE BY CURRENT ID
+@community_event_image_routes.route('/<int:id>')
+def get_image_details(id):
+    image = CommunityEventImage.query.get(id).to_dict()
+    if not image:
+        return {
+            "errors": "Image couldn't be found",
+            "status_code": 404
+        }, 404
+    return image
