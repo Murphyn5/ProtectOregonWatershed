43a7122bca064bc738089406adabb7e6d10639d5
cmstucke <cmstucke@gmail.com>
2024-01-29 12:07:59 -0500

    foot elements light mode

--- src/app/components/main/page.tsx
+++ src/app/components/main/page.tsx
@@ -7,6 +7,7 @@ const Main = () => {
   const defaultSection = "flex justify-center w-full"
   const subHeading = "text-darkText text-7xl font-semibold m-0 p-0 mb-5";
   const whiteParagraph = "text-white text-xl"
+  const darkParagraph = "text-darkText text-xl font-semibold"
   const callText = "text-white text-5xl font-bold my-8 px-5 text-nowrap";
   const callSplash = "flex w-full justify-center"
   const readMore = "bg-midnightGreen px-5 py-2 mb-5"
@@ -54,33 +55,36 @@ const Main = () => {
 
       <section className="flex w-full bg-white">
         <div className="flex flex-col items-center w-2/5 bg-persianGreen">
-          <h3>join us</h3>
-          <button>[join button]</button>
+          <h3 className={`${callText}`}>join us</h3>
+          <button className={`${readMore} ${whiteParagraph}`}>[join button]</button>
         </div>
-        <div className="flex justify-center w-3/5">
-          <h3>[join image]</h3>
-        </div>
+        <img
+          src="/join.jpg"
+          alt="main image"
+          // className="w-full h-full object-cover fixed -z-10 m-0 p-0"
+          className="w-3/5 object-cover h-44 object-top"
+        />
       </section>
 
-      <section className="flex w-full">
-        <div className="flex items-center w-2/5">
-          <h4>[site contacts]</h4>
+      <section className="flex w-full p-5 bg-white">
+        <div className="flex items-start w-2/5">
+          <h4 className={`${darkParagraph}`}>[site contacts]</h4>
         </div>
-        <div className="flex items-center w-3/5">
-          <div className="flex flex-col w-3/5">
-            <h5>[spray notification signup]</h5>
-            <form className="flex flex-col items-start">
+        <div className="flex items-start w-3/5">
+          <div className="flex flex-col w-3/5 gap-4">
+            <h5 className={`${darkParagraph}`}>[spray notification signup]</h5>
+            <form className="flex flex-col items-start gap-2">
               <input id="spray-notification-input" type="email" />
-              <button className="main-button">[email submit]</button>
+              <button className={`${darkParagraph}`}>[email submit]</button>
             </form>
           </div>
           <div className="additional">
-            <h5>[additional links]</h5>
+            <h5 className={`${darkParagraph}`}>[additional links]</h5>
           </div>
         </div>
       </section>
 
-      <section>
+      <section className="bg-persianGreen p-1 flex justify-center">
         <h6>[footer]</h6>
       </section>
 
