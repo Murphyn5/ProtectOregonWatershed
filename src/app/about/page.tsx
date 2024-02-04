d0ee53637697ec30932aef70ab63daa850fd0bcc
Sing Cheng <Sing.cheng@outlook.com>
2024-02-04 16:12:39 -0500

    styled about and action page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,27 +1,58 @@
 import { Button } from '@nextui-org/react';
-// import Test from '../components/test_component/test';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Fragment } from 'react';
 
 export default function About() {
   return (
-    <div style={{ padding: '20px' }}>
-      <div>About</div>
-      <div>
-        The Beaver Creek watershed is being threatened by a toxic herbicide
-        spray over a clear cut near vital water sources. Please help us stop
-        this crisis!
+    <div className="">
+      <div className="pb-3 w-full h-40 flex flex-col items-center justify-center gap-2 bg-persianGreen">
+        <h1 className="py-3 text-6xl text-white">About</h1>
       </div>
-      <div>Vision</div>
-      <div>
-        CARING FOR OUR WORLD! We believe that everyone deserves to live in a
-        clean. safe & sustainable environment for the present and the next
-        generations to come. Protect endangered species Protect the wilderness
-        Advocate for clean beaches and riversides Keep our air and water clean
-        Ensure a clean energy future Curb climate change Keep the pressure on
-        politicians and corporations to ensure safe and healthy communities
+      <div className='w-full py-10 flex flex-col items-center justify-center'>
+        <p className="text-center">
+          The Beaver Creek watershed is being threatened by a toxic herbicide spray over a clear cut near vital water sources. Please help us stop this crisis!
+        </p>
+        <Link href="/action" className='bg-persianGreen shadow-md p-3 mt-3'>
+          <p className="text-white text-xl ">We Need Your Support!</p>
+        </Link>
       </div>
-      <div>Mission</div>
-      <div>EMPOWERING THE COMMUNITY TOGETHER WE CAN MAKE A DIFFERENCE!</div>
-      <div>PHOTOS!</div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="max-w-1/2">
+          <Image src="/mission.jpeg" alt="Our Vision" width={1000} height={300} />
+        </div>
+        <div className="bg-white shadow-md rounded-lg max-w-1/2 flex flex-col justify-center items-center">
+          <div className="text-5xl font-semibold text-grey-600 mb-2">Our Mission</div>
+          <p className="text-gray-700 text-lg p-10">
+            EMPOWERING THE COMMUNITY TOGETHER WE CAN MAKE A DIFFERENCE!
+          </p>
+        </div>
+      </div>
+
+
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="bg-white shadow-md rounded-lg max-w-1/2 flex flex-col justify-center items-center">
+          <div className="text-5xl font-semibold text-grey-600 mb-2">Our Vision</div>
+          <p className="text-gray-700 text-lg p-10">
+            CARING FOR OUR WORLD! We believe that everyone deserves to live in a clean, safe & sustainable environment for the present and the next generations to come. Protect endangered species, Protect the wilderness, Advocate for clean beaches and riversides, Keep our air and water clean, Ensure a clean energy future, Curb climate change, Keep the pressure on politicians and corporations to ensure safe and healthy communities.
+          </p>
+        </div>
+        <div className="max-w-1/2">
+          <Image src="/vision.jpeg" alt="Our Vision" width={1000} height={300} />
+        </div>
+      </div>
+
+
+
+      <div className="mt-12 flex justify-center items-center">
+        <div className="text-4xl font-semibold text-grey-900 mb-2">PHOTOS!</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {/* {photos.map((photo) => (
+            <img key={photo.id} src={photo.src} alt={photo.alt} className="w-full h-64 object-cover rounded-md" />
+          ))} */}
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/action/actionData.js
+++ src/app/action/actionData.js
@@ -1,63 +1,79 @@
-WriteList = [
+const WriteList = [
   {
     name: 'Governor Tina Kotek',
-    email: '',
+    email: 'tkotek@hotmail.com',
   },
   {
     name: 'Oregon State Senator Dick Anderson',
-    email: '',
+    email: 'Sen.DickAnderson@oregonlegislature.gov',
   },
-  { name: 'Senator Jeff Merkley', email: '' },
-  { name: 'Senator Ron Wyden', email: '' },
+  { name: 'Senator Jeff Merkley', email: 'Jeff@jeffmerkley.com' },
+  { name: 'Senator Ron Wyden', email: '', website: 'https://www.wyden.senate.gov/' },
   {
     name: 'Representative David Gomberg',
-    email: '',
+    email: 'Rep.DavidGomberg@oregonlegislature.gov',
   },
   {
     name: 'Commissioner Casey Miller',
-    email: '',
+    email: 'clmiller@co.lincoln.or.us',
   },
   {
     name: 'Commissioner Kaety Jacobson',
-    email: '',
+    email: 'kjacobson@co.lincoln.or.us',
   },
   {
     name: 'Commissioner Claire Hall',
-    email: '',
+    email: 'cehall@co.lincoln.or.usure.gov',
   },
   {
     name: 'Land Owner, Sorn Nymark',
-    email: '',
+    email: 'info@swwregagents.com',
   },
   {
     name: 'Land Management Forester, Tyrol Forfar',
-    email: '',
+    email: 'tyrol@hficonsultants.com',
   },
   {
     name: 'Land Management President Tim Halme',
-    email: '',
+    email: 'tim@hficonsultants.com',
   },
   {
     name: 'Land Management Consulting Forester Travis Anderberg',
-    email: '',
+    email: 'travis@hficonsultant.com',
   },
   {
-    name: 'Click for link to HFI Consultants contact page',
+    name: 'HFI Consultants contact page',
     email: '',
+    website: 'https://hficonsultants.com/contact/'
   },
   {
     name: 'Aerial Spray Company, Antony Ghidossi',
-    email: '',
+    email: 'anthony@pacificservices.com',
   },
   {
     name: 'OR Dept. of Forestry, Rieghly Sitton',
-    email: '',
+    email: 'rieghly.k.sitton@odf.oregon.gov',
   },
 ];
-SampleLetters = [
-  {
-    sample1: { link: 'google.com', src: '/main.jpg' },
-    sample2: { link: 'google.com', src: '/main.jpg' },
-    sample3: { link: 'google.com', src: '/main.jpg' },
-  },
+const SampleLetters = [
+  { name: 'Sample Letter #1', link: 'https://docs.google.com/document/d/1e41A2lnOsdna_cGQCZlO24ihVLTGlT5gnEbpSnbpkSE/', src: '/sample-letter-1.jpeg' },
+  { name: 'Sample Letter #2', link: 'https://docs.google.com/document/d/1KpKx4qNnrdNZcPjEmYThrDY6nwg-NMOJHIe3k1yHUBE/', src: '/sample-letter-2.jpeg' },
+  { name: 'Sample Letter #3', link: 'https://docs.google.com/document/d/15gZzl820KunV2SUmUgIUeKw7wvGtoUjAIFB9SK8XnNU/', src: '/sample-letter-3.jpeg' },
+  { name: 'Sample Letter #4', link: 'https://docs.google.com/document/d/1e41A2lnOsdna_cGQCZlO24ihVLTGlT5gnEbpSnbpkSE/', src: '/write-sample-1.jpeg' },
+  { name: 'Sample Letter #5', link: 'https://docs.google.com/document/d/1KpKx4qNnrdNZcPjEmYThrDY6nwg-NMOJHIe3k1yHUBE/', src: '/write-sample-2.jpeg' },
+  { name: 'Sample Letter #6', link: 'https://docs.google.com/document/d/15gZzl820KunV2SUmUgIUeKw7wvGtoUjAIFB9SK8XnNU/', src: '/write-sample-3.jpeg' },
 ];
+
+const MediaList = [
+  { name: 'The Wave', contactName: 'Stephanie Linn', email: 'thewave937studio@gmail.com', phone: ["(541) 265-6000", "(541) 265-3000"], website: '', address: '', mediaType: '93.7 FM' },
+  { name: 'Yaquina Bay Communications', contactName: 'Dave Miller', email: 'http://dmiller@ybcradio.com/', phone: ["(541) 265-2266"], website: '', address: '906 S.W. Alder, Newport, OR', mediaType: '102.7 FM 1310AM 92.7 FM 1400 AM 96.7 FM' },
+  { name: 'Otter Rock Radio', contactName: 'Jeffrey Montgomery', email: '', phone: ["(541) 265-5000", "(541) 283-1007"], website: 'https://info@otterrockradio.com/', address: '1676 N Coast Hwy, Newport, OR 97365', mediaType: '100.7 FM' },
+  { name: 'KYAQ Radio', contactName: '', email: '', phone: ["(541) 272-5200"], website: 'https://kyaq.org/about-kyaq/mapdirections/', address: '324 N Coast Hwy Ste 6, Newport, OR 97365, PO Box 1664, Newport OR 97365', mediaType: '91.7 FM' },
+  { name: 'Yachats News', contactName: 'Quinton Smith', email: 'yachatsgazette@gmail.com', phone: ["(541) 265-5000"], website: '', address: 'PO Box 284, Yachats, OR 97498', mediaType: 'on-line media' },
+  { name: 'Lincoln News', contactName: 'Dave Morgan', email: 'news@newslincolncounty.com', phone: ["(541) 351-1408"], website: '', address: '', mediaType: 'on-line media' },
+  { name: 'Information Station', contactName: 'Wendy Knudson', email: 'wendy.j.rush@gmail.com', phone: ["(541) 961-5819"], website: '', address: 'PO Box 362, Waldport, OR 97394', mediaType: 'on-line media' },
+  { name: 'News/Times', contactName: 'Steve Card', email: 'scard@newportnewstimes.com', phone: ["(541) 265-8571 ext 224", "(541)265-3862"], website: '', address: '831 NE Avery St, Newport OR 97365', mediaType: 'on-line and print media' },
+];
+
+
+export { WriteList, SampleLetters, MediaList };
