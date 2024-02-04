b3754ef3701a207b088e67290615977794af4eff
Sing Cheng <Sing.cheng@outlook.com>
2024-02-03 19:02:12 -0500

    footer, about and action

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,16 +4,24 @@ import { Button } from '@nextui-org/react';
 export default function About() {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Protect Oregon Watershed&apos;s About, vision &amp; mission page</h1>
-      <div className="flex flex-wrap gap-4 items-center">
-        <Button color="default">Default</Button>
-        <Button color="primary">Primary</Button>
-        <Button color="secondary">Secondary</Button>
-        <Button color="success">Success</Button>
-        <Button color="warning">Warning</Button>
-        <Button color="danger">Danger</Button>
+      <div>About</div>
+      <div>
+        The Beaver Creek watershed is being threatened by a toxic herbicide
+        spray over a clear cut near vital water sources. Please help us stop
+        this crisis!
       </div>
-      {/* <Test /> */}
+      <div>Vision</div>
+      <div>
+        CARING FOR OUR WORLD! We believe that everyone deserves to live in a
+        clean. safe & sustainable environment for the present and the next
+        generations to come. Protect endangered species Protect the wilderness
+        Advocate for clean beaches and riversides Keep our air and water clean
+        Ensure a clean energy future Curb climate change Keep the pressure on
+        politicians and corporations to ensure safe and healthy communities
+      </div>
+      <div>Mission</div>
+      <div>EMPOWERING THE COMMUNITY TOGETHER WE CAN MAKE A DIFFERENCE!</div>
+      <div>PHOTOS!</div>
     </div>
   );
 }
