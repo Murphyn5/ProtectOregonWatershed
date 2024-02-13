cb86a12c5327f944582c8f213a116b8b89481ead
nick <nlimurphy@gmail.com>
2024-02-13 06:06:43 -0700

    changed next.config.js file

--- next.config.js
+++ next.config.js
@@ -7,11 +7,10 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5324/api/:path*'
-            : `${process.env.POSTGRES_URL}/api/:path*`,
+            : '/api/:path*',
       },
     ]
   },
 }
 
-module.exports = nextConfig;
-//
+module.exports = nextConfig
